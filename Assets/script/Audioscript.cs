93d0c2b7ca17e870fd340df97999617431247a1c
redphoenix2000 <jean.gabriel.mathe@gmail.com>
2017-12-29 02:36:27 +0100

    version_12
    audio

--- Assets/script/Audioscript.cs
+++ Assets/script/Audioscript.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Audioscript : MonoBehaviour {
 
     void Awake()
     {
-        GameObject[] music = GameObject.FindGameObjectsWithTag("music");
-        if (music.Length > 1)
+        
+        GameObject []music = GameObject.FindGameObjectsWithTag("music");
+        if (music.GetLength(0) > 1)
         {
-            Destroy(this.gameObject);
+            DestroyImmediate(music[0]);
+            music[0] = this.gameObject;
+            music[1] = null;
         }
         else
         {
