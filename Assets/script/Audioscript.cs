a518424d01475ddbc9dfb880ac13af43c519f355
redphoenix2000 <jean.gabriel.mathe@gmail.com>
2017-12-29 01:05:23 +0100

    version_11
    music

---
+++ Assets/script/Audioscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audioscript : MonoBehaviour {
+
+    void Awake()
+    {
+        GameObject[] music = GameObject.FindGameObjectsWithTag("music");
+        if (music.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        
+    }
+}
